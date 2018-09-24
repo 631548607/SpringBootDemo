a085d9f80b6cbd65a05f0451735a5cde36e13f2d
hongchao <631548607@qq.com>
2018-09-24 21:20:21 +0800

    项目初始

---
+++ src/main/java/com/demo/hc/HcApplication.java
@@ -0,0 +1,12 @@
+package com.demo.hc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcApplication.class, args);
+	}
+}
